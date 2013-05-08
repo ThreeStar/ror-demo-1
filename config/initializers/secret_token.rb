608282ba83ad80a9b87d38335024de17bf7c4fb0
Blues <blues@jpluscorp.com>
2013-05-08 17:08:45 +0800

    init commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorDemo1::Application.config.secret_token = '220f8150d185668a765e6233f84d115e1d6d52547283eb7a06a2f4e3c88782bc9540f60ee93521f8de9f0df77f64e1afd7c670300a70eb71898e70b39e77b553'
